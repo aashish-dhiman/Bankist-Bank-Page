238ab00da46b1cdfc14709a3e5888d2a78e38bd5
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-01-30 19:25:33 +0530

    added balance and transaction

--- script/script.js
+++ script/script.js
@@ -1,83 +1,124 @@
-'use strict';
+"use strict";
 
 // Data
 const account1 = {
-  owner: 'Aashish Dhiman',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2,
-  pin: 1111,
+    owner: "Aashish Dhiman",
+    movements: [2000, 45000, -4000, 3000, -650, -1300, 70000, 1300],
+    interestRate: 1.2,
+    pin: 1111,
 };
 
 const account2 = {
-  owner: 'Piyush Dhiman',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+    owner: "Piyush Dhiman",
+    movements: [5000, 34000, -1500, -790, -32100, -10000, 85000, -300],
+    interestRate: 1.5,
+    pin: 2222,
 };
 
 const account3 = {
-  owner: 'Raju Rastogi',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
+    owner: "Raju Rastogi",
+    movements: [20000, -2000, 340, -3000, -200, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
 };
 
 const account4 = {
-  owner: 'Ujjawal Saini',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
+    owner: "Ujjawal Saini",
+    movements: [4300, 10000, 700, -500, 9000],
+    interestRate: 1,
+    pin: 4444,
 };
 
+//array of accounts objects-->
 const accounts = [account1, account2, account3, account4];
 
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+// Elements-->
+const labelWelcome = document.querySelector(".welcome");
+const labelDate = document.querySelector(".date");
+const labelBalance = document.querySelector(".balance__value");
+const labelSumIn = document.querySelector(".summary__value--in");
+const labelSumOut = document.querySelector(".summary__value--out");
+const labelSumInterest = document.querySelector(".summary__value--interest");
+const labelTimer = document.querySelector(".timer");
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
+const containerApp = document.querySelector(".app");
+const containerMovements = document.querySelector(".movements");
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+const btnLogin = document.querySelector(".login__btn");
+const btnTransfer = document.querySelector(".form__btn--transfer");
+const btnLoan = document.querySelector(".form__btn--loan");
+const btnClose = document.querySelector(".form__btn--close");
+const btnSort = document.querySelector(".btn--sort");
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+const inputLoginUsername = document.querySelector(".login__input--user");
+const inputLoginPin = document.querySelector(".login__input--pin");
+const inputTransferTo = document.querySelector(".form__input--to");
+const inputTransferAmount = document.querySelector(".form__input--amount");
+const inputLoanAmount = document.querySelector(".form__input--loan-amount");
+const inputCloseUsername = document.querySelector(".form__input--user");
+const inputClosePin = document.querySelector(".form__input--pin");
 
-
-//functions
+//functions-->
 
 // function to add transaction history
 const displayMovements = function (movements) {
-  containerMovements.innerHTML = " ";
+    containerMovements.innerHTML = " ";
 
-  movements.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    movements.forEach(function (mov, i) {
+        const type = mov > 0 ? "deposit" : "withdrawal";
 
-    //html template to be added
-    const html = `
-    <div class="movements__row">
-      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-      <div class="movements__date">0 days ago</div>
-      <div class="movements__value">${mov} &#8377;</div>
-    </div>
-  `;
-    containerMovements.insertAdjacentHTML("afterbegin", html);
-  });
+        //html template to be added
+        const html = `
+        <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+            i + 1
+        } ${type}</div>
+        <div class="movements__date">0 days ago</div>
+        <div class="movements__value">${mov} &#8377;</div>
+        </div>`;
+        containerMovements.insertAdjacentHTML("afterbegin", html);
+    });
 };
 
 displayMovements(account1.movements);
 
+//function to create username for accounts as initials of the name-->
+//for each user we create a new property username
+//we split the owner name around ' ' then map function returns new array of first word of name
+//last we join the new array returned by map using join function
+const username = function (account) {
+    account.forEach(function (user) {
+        user.username = user.owner
+            .toLowerCase()
+            .split(" ")
+            .map((name) => name[0])
+            .join("");
+    });
+};
+username(accounts);
+// console.log(accounts);
+
+//aliter to compute username
+// const username = function (account) {
+//     account.forEach(function (user) {
+//         user.username = user.owner
+//             .toLowerCase()
+//             .split(" ")
+//             .map(function (name) {
+//                 return name[0];
+//             })
+//             .join("");
+//     });
+// };
+
+//function to display total balance using reduce() method-->
+const displayBalance = function (movements) {
+    const balance = movements.reduce(function (acc, mov) {
+        return acc + mov;
+    }, 0);
+    //aliter using arrow function
+    // const balance = movements.reduce((acc, mov) => acc + mov, 0);
+
+    labelBalance.textContent = `${balance} ₹`;
+};
+displayBalance(account1.movements);
